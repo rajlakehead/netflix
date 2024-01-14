25dbd043c76c7e89ce84c9a214ace49d4f2b2e89
Raj Patel <patelraj24365@gmail.com>
2024-01-14 17:48:47 -0500

    navbar added

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,17 +1,17 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
-//mport GithubSignInButton from "@/app/components/GithubSignInButton";
-//import GoogleSignInButton from "@/app/components/GoogleSignInButton";
+import GithubSignInButton from "@/app/components/GithubSignInButton";
+import GoogleSignInButton from "@/app/components/GoogleSignInButton";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/utils/auth";
 import { redirect } from "next/navigation";
 
 export default async function SignUp() {
-  //const session = await getServerSession(authOptions);
-  //if (session) {
-    //return redirect("/home");
-  //}
+  const session = await getServerSession(authOptions);
+  if (session) {
+    return redirect("/home");
+  }
   return (
     <div className="mt-24  rounded bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
       <form method="post" action="/api/auth/signin">
@@ -41,7 +41,8 @@ export default async function SignUp() {
       </div>
 
       <div className="flex w-full justify-center items-center gap-x-3 mt-6">
-        
+        <GithubSignInButton />
+        <GoogleSignInButton/>
       </div>
     </div>
   );
---
+++ app/home/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+export default function Home() {
+  return (  
+    <div>
+        <h1>Raj</h1>
+    </div>
+  )
+}
